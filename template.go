fcac85442a084cc4442ba6354a994fe236f0c2c3
phipus <phipu.s@icloud.com>
2024-02-18 15:52:42 +0100

    add render html function

--- template.go
+++ template.go
@@ -1,7 +1,15 @@
 package tplexpr
 
+import "net/http"
+
 type Vars = map[string]Value
 
 type Template struct {
 	Code []Instr
+}
+
+func (c *Context) RenderTemplateHTML(status int, w http.ResponseWriter, name string, vars Vars) error {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(status)
+	return c.EvalTemplateWriter(name, vars, w)
 }
